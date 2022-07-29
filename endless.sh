bd4ddaa093543d1de47ab323405f7cbd08e0064c
notfreshmbp <1195056983@qq.com>
2022-07-29 18:08:01 +0800

    use shell to start doorman

--- endless.sh
+++ endless.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set j=2
-while true
-do
-        let "j=j+1"
-        echo "----------j is $j--------------"
-        sleep 3s
-done
+#set j=2
+#while true
+#do
+#        let "j=j+1"
+#        echo "----------j is $j--------------"
+#        sleep 3s
+#done
+export PORT=6000
+./doorman -config=./config/config.yml -port=$PORT -debug_port=$(expr $PORT + 50) -etcd_endpoints=http://etcd:2379 -master_election_lock=/doorman.master -log_dir=/doorman_log_dir  -alsologtostderr
